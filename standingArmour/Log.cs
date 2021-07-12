bc200c00c1c3d0e406eb36dcc5b7045a436306fc
obergad <amoallmoa@Gaming-Masterpeice.localdomain>
2021-07-12 16:29:16 -0700

    1.0.0 release

--- standingArmour/Log.cs
+++ standingArmour/Log.cs
@@ -1,6 +1,6 @@
 ﻿using BepInEx.Logging;
 
-namespace ExamplePlugin
+namespace StandingArmour
 {
     internal static class Log
     {
--- standingArmour/standingArmour.cs
+++ standingArmour/standingArmour.cs
@@ -4,7 +4,7 @@ using R2API.Utils;
 using RoR2;
 using UnityEngine;
 
-namespace ExamplePlugin
+namespace StandingArmour
 {
 	//This is an example plugin that can be put in BepInEx/plugins/ExamplePlugin/ExamplePlugin.dll to test out.
     //It's a small plugin that adds a relatively simple item to the game, and gives you that item whenever you press F2.
@@ -21,13 +21,13 @@ namespace ExamplePlugin
 	
 	//This is the main declaration of our plugin class. BepInEx searches for all classes inheriting from BaseUnityPlugin to initialize on startup.
     //BaseUnityPlugin itself inherits from MonoBehaviour, so you can use this as a reference for what you can declare and use in your plugin class: https://docs.unity3d.com/ScriptReference/MonoBehaviour.html
-    public class ExamplePlugin : BaseUnityPlugin
+    public class StandingArmour : BaseUnityPlugin
 	{
         //The Plugin GUID should be a unique ID for this plugin, which is human readable (as it is used in places like the config).
         //If we see this PluginGUID as it is on thunderstore, we will deprecate this mod. Change the PluginAuthor and the PluginName !
         public const string PluginGUID = PluginAuthor + "." + PluginName;
-        public const string PluginAuthor = "AuthorName";
-        public const string PluginName = "ExamplePlugin";
+        public const string PluginAuthor = "Snorlaxius";
+        public const string PluginName = "StandingArmour";
         public const string PluginVersion = "1.0.0";
 
 		//We need our item definition to persist through our functions, and therefore make it a class field.
@@ -43,16 +43,18 @@ namespace ExamplePlugin
             myItemDef = ScriptableObject.CreateInstance<ItemDef>();
 
             // Language Tokens, check AddTokens() below.
-            myItemDef.name = "EXAMPLE_CLOAKONKILL_NAME";
-            myItemDef.nameToken = "EXAMPLE_CLOAKONKILL_NAME";
-            myItemDef.pickupToken = "EXAMPLE_CLOAKONKILL_PICKUP";
-            myItemDef.descriptionToken = "EXAMPLE_CLOAKONKILL_DESC";
-            myItemDef.loreToken = "EXAMPLE_CLOAKONKILL_LORE";
+            myItemDef.name = "STANDINGARMOUR_NAME";
+            myItemDef.nameToken = "STANDINGARMOUR_NAME";
+            myItemDef.pickupToken = "STANDINGARMOUR_PICKUP";
+            myItemDef.descriptionToken = "STANDINGARMOUR_DESC";
+            myItemDef.loreToken = "STANDINGARMOUR_LORE";
 
             //The tier determines what rarity the item is:
             //Tier1=white, Tier2=green, Tier3=red, Lunar=Lunar, Boss=yellow,
             //and finally NoTier is generally used for helper items, like the tonic affliction
-            myItemDef.tier = ItemTier.Tier2;
+            myItemDef.tier = ItemTier.Tier3;
+            myItemDef.tags = new ItemTag[] { ItemTag.Utility , ItemTag.AIBlacklist };
+
 
             //You can create your own icons and prefabs through assetbundles, but to keep this boilerplate brief, we'll be using question marks.
             myItemDef.pickupIconSprite = Resources.Load<Sprite>("Textures/MiscIcons/texMysteryIcon");
@@ -79,6 +81,27 @@ namespace ExamplePlugin
             //But now we have defined an item, but it doesn't do anything yet. So we'll need to define that ourselves.
             GlobalEventManager.onCharacterDeathGlobal += GlobalEventManager_onCharacterDeathGlobal;
 
+            //====================================================================================
+            /*CharacterBody player =  PlayerCharacterMasterController.instances[0].master.GetBody(); 
+            bool isNotMoving = player.GetNotMoving();
+            bool buffApplied = false; 
+            if (isNotMoving == true)
+            {
+                int garbCount = player.inventory.GetItemCount(myItemDef.itemIndex);
+                if(garbCount < 0 && buffApplied == false)
+                {
+                    buffApplied = true;
+                    player.baseArmor += (garbCount * 2 + 10);
+                    player.RecalculateStats(); 
+                }
+            }
+            else
+            {
+                buffApplied = false; 
+            }
+            */
+            //====================================================================================
+
             // This line of log will appear in the bepinex console when the Awake method is done.
             Log.LogInfo(nameof(Awake) + " done.");
         }
@@ -86,6 +109,8 @@ namespace ExamplePlugin
         private void GlobalEventManager_onCharacterDeathGlobal(DamageReport report)
         {
             //If a character was killed by the world, we shouldn't do anything.
+
+
             if (!report.attacker || !report.attackerBody )
                 return;
             
@@ -95,14 +120,20 @@ namespace ExamplePlugin
             if (attacker.inventory)
             {
                 //store the amount of our item we have
+                bool isNotMoving = attacker.GetNotMoving();
                 int garbCount = attacker.inventory.GetItemCount(myItemDef.itemIndex);
-                if (garbCount > 0 &&
-                    //Roll for our 50% chance.
-                    Util.CheckRoll(50, attacker.master))
+                if (garbCount > 0 && garbCount < 5 && isNotMoving == true)
                 {
-                    //Since we passed all checks, we now give our attacker the cloaked buff.
-                    //Note how we are scaling the buff duration depending on the number of the custom item in our inventory.
-                    attacker.AddTimedBuff(RoR2Content.Buffs.Cloak, 3 + garbCount);
+                    attacker.AddTimedBuff(RoR2Content.Buffs.SmallArmorBoost, 1 + garbCount);
+                }
+                else if (garbCount > 5 && garbCount < 10 && isNotMoving == true)
+                {
+                    attacker.AddTimedBuff(RoR2Content.Buffs.ArmorBoost, 1 + garbCount);
+
+                }
+                 else if (garbCount > 10 && isNotMoving == true)
+                {
+                    attacker.AddTimedBuff(RoR2Content.Buffs.ElephantArmorBoost, 1 + garbCount);
                 }
             }
         }
@@ -111,16 +142,16 @@ namespace ExamplePlugin
         private void AddTokens()
         {
             //The Name should be self explanatory
-            LanguageAPI.Add("EXAMPLE_CLOAKONKILL_NAME", "Cuthroat's Garb");
+            LanguageAPI.Add("STANDINGARMOUR_NAME", "Sentinel's Armour");
 
             //The Pickup is the short text that appears when you first pick this up. This text should be short and to the point, numbers are generally ommited.
-            LanguageAPI.Add("EXAMPLE_CLOAKONKILL_PICKUP", "Chance to cloak on kill");
+            LanguageAPI.Add("STANDINGARMOUR_PICKUP", "If Standing Still Gain Armor Boost on Kill");
 
             //The Description is where you put the actual numbers and give an advanced description.
-            LanguageAPI.Add("EXAMPLE_CLOAKONKILL_DESC", "Whenever you <style=cIsDamage>kill an enemy</style>, you have a <style=cIsUtility>5%</style> chance to cloak for <style=cIsUtility>4s</style> <style=cStack>(+1s per stack)</style.");
+            LanguageAPI.Add("STANDINGARMOUR_DESC", "After getting a kill while standing still gain <style=cIsUtility>100 armor</style><style=cStack>200 if more than 5 stacks and 500 if more than 10 stacks</style> for <style=cIsUtility>1s<style=cStack> (+1s per stack)</style>.");
             
             //The Lore is, well, flavor. You can write pretty much whatever you want here.
-            LanguageAPI.Add("EXAMPLE_CLOAKONKILL_LORE", "Those who visit in the night are either praying for a favour, or preying on a neighbour.");
+            LanguageAPI.Add("STANDINGARMOUR_LORE", "Those who are patient are stronger.");
         }
 
         //The Update() method is run on every frame of the game.
